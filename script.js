963a2c1d51bde132efee495c4e8405b655d09c29
Philneeves <p.neeves@gmail.com>
2017-01-10 16:00:33 +0000

    if/else added in

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ var damageThisRound = Math.floor(Math.random() * 5 + 1);
 var totalDamage = 0;
 
 while (slaying) {
+    if (youHit) {
+        console.log("Congratulations, you slayed the dragon")
+    } else {
+        console.log("You lost!");
+    }
     slaying = false;
 }
